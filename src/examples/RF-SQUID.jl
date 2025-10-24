abc41eda7a37ef6eb0a6e50e8be7e78b4fe64890
WilliamCam <78771433+WilliamCam@users.noreply.github.com>
2025-10-24 09:14:29 +0800

    Update RF-SQUID.jl

--- src/examples/RF-SQUID.jl
+++ src/examples/RF-SQUID.jl
@@ -4,12 +4,20 @@
 using JosephsonLoops 
 const jls = JosephsonLoops
 
-#open the circuit netlist file named RF-SQUID.jld2, to create a new circuit netlist we can use jls.new_netlist()
-jls.open_file("src//examples//RF-SQUID.jld2")
+### Write Netlist
+#Define componeents in each loop
+loops = [
+["J1"],
+["L2","C1"],
+["C1","R1"],
+["R1","I1"]]
 
+coupling = [(1,2)]
+
+circuit = jls.process_netlist(loops, coupling)
 
 #cirucit model ODAE system and initial condition vector are created.
-model, u0 = jls.build_circuit()
+model, u0 = jls.build_circuit(circuit)
 
 # we set the values of circuit parameters, for any parameters not specified; default values will be assigned.
 
@@ -19,17 +27,17 @@ R₀ = 5.0
 
 βc  = 2*pi/Φ₀ * I₀ * R₀^2
 βL = 2*pi/Φ₀ * I₀
+fd = 100e6
 
 ps = [
-    jls.loop4.sys.ω => 2*pi*100.0e+6
-    jls.loop4.sys.I => 0.8*I₀
+    jls.loop4.sys.ω => 2*pi*fd
+    jls.loop4.sys.I => 1.0*I₀
     jls.J1.sys.I0 => I₀
     jls.J1.sys.R => R₀
     jls.J1.sys.C => 0.01/βc
-    jls.J1.sys.L => 1.0/βc
+    jls.J1.sys.L => 2.0/βL
     jls.R1.sys.R => 50.0
     jls.C1.sys.C => 2.0/βc
-    jls.L1.sys.L => 2.0/βL
     jls.L2.sys.L => 100.0/βL
     jls.M12.sys.L => 8.0/βL
     jls.loop1.sys.Φₑ => 0.5*Φ₀
@@ -41,18 +49,8 @@ saveat = LinRange(tspan[2]/10.0, tspan[2], 10000)
 sol = jls.tsolve(model, u0, tspan, ps; saveat = saveat)
 jls.tplot(sol, jls.R1, units="volts")
 
-##Benchmarking
-using ModelingToolkit
-using BenchmarkTools
-using DifferentialEquations
-using Profile
-
-prob = ODEProblem(model, u0, tspan, ps, saveat = saveat)
-@benchmark sol = solve(prob, Rodas5())
-
-
-
 ## Parameter Sweeps
+using Plots
 Φspan = (0.0, 2.0*Φ₀)
 ensemble_sol = jls.ensemble_parameter_sweep(model, u0, tspan, Φspan, ps, jls.loop1.sys.Φₑ, jls.R1, saveat = saveat)
 plot(ensemble_sol.u)
